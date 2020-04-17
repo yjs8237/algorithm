1209fd9ebd97f359a2233f1d8201ed6bd8186a6b
greatyun <yjs8237@gmail.com>
2020-04-18 00:42:05 +0900

    이진트리 검색 방법 메소드 작성

--- src/main/java/com/greatyun/algorithm/binaryTree/BinaryTreeSearch.java
+++ src/main/java/com/greatyun/algorithm/binaryTree/BinaryTreeSearch.java
@@ -35,4 +35,34 @@ public class BinaryTreeSearch {
         }
     }
 
+    public Node searchNode(int data) {
+        return search(root , data);
+    }
+
+    /**
+     * Binary Tree 검색
+     * 이진트리가 루트 기준으로 왼쪽은 작은 값 , 오른쪽은 큰 값 으로 트리가 구성되어 있다는 가정하에
+     * 아래 메소드는 정상동작한다.
+     * @param root
+     * @param data
+     * @return
+     */
+    private Node search(Node root, int data) {
+        if(root == null ) return null;
+
+        while(true) {
+            if(root.getData() == data) {
+                // 검색 노드 찾음
+                return root;
+            }
+            if(root.getData() > data) {
+                // 자신의 데이터가 더 크다면 찾고자 하는 노드는 내 기준으로 왼쪽에 있다.
+                root = root.getLeft();
+            } else {
+                // 자신의 데이터가 더 작다면 찾고자 하는 노드는 내 기준으로 오른쪽에 있다.
+                root = root.getRight();
+            }
+        }
+    }
+
 }
