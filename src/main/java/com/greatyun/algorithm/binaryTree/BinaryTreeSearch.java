9d3a70624776f04f7223b0ecefe2a4b3a216246c
greatyun <yjs8237@gmail.com>
2020-04-25 17:27:22 +0900

    스터디 내용물 투척

--- src/main/java/com/greatyun/algorithm/binaryTree/BinaryTreeSearch.java
+++ src/main/java/com/greatyun/algorithm/binaryTree/BinaryTreeSearch.java
@@ -5,6 +5,9 @@ import com.greatyun.algorithm.LeetCode.ConstructBinarySearchTreefromPreorderTrav
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 @Slf4j
 public class BinaryTreeSearch {
@@ -79,6 +82,49 @@ public class BinaryTreeSearch {
                 root = root.getRight();
             }
         }
+    }
+
+
+    /**
+     * 주어진 이진 트리가 이진 검색 트리인지 확인하는 메소드
+     * @param root
+     * @return
+     */
+    public boolean isBinaryTree(Node root) {
+        return searchInOrder(root);
+
+    }
+
+    Integer prev;   // 이진검색 트리의 이전 값을 저장하는 Integer 객체
+    private boolean searchInOrder(Node root) {
+        if(root != null) {
+            // 재귀 함수 호출 결과가 false 이면 false 를 리턴해서 마무리
+            if(!searchInOrder(root.getLeft())) {
+                return false;
+            }
+            System.out.println("prev : " + prev + " , data : " + root.getData());
+            // 이전 데이터가 현재의 데이터보다 크면 이진 검색 트리가 아니다.
+            // inorder 방식으로 이진트리를 순회하면 오름차순으로 데이터를 읽게 된다.
+            if(prev != null && prev > root.getData()) {
+                return false;
+            }
+            prev = root.getData();
+            if(!searchInOrder(root.getRight())) {
+                return false;
+            }
+        }
+        // 메소드 호출이 끝나면 true 리턴
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        BinaryTreeSearch tree = new BinaryTreeSearch();
+        int [] arr = {1,2,3,4,5};
+        Node root = tree.makeTree(arr, 0, arr.length-1);
+        System.out.println(root.getRight().getRight().getData());
+//        root.getRight().getRight().setRight(new Node((0)));
+        System.out.println(tree.isBinaryTree(root));
     }
 
 }
