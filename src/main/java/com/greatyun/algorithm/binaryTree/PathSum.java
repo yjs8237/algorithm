06c3910ff305b61dc7e132c9f0883e943ed5d4ff
greatyun <yjs8237@gmail.com>
2020-05-07 00:40:39 +0900

    릿코드 문제풀이

--- src/main/java/com/greatyun/algorithm/binaryTree/PathSum.java
+++ src/main/java/com/greatyun/algorithm/binaryTree/PathSum.java
@@ -30,6 +30,24 @@ public class PathSum {
         return search(root.left , subSum + root.val , sum) || search(root.right , subSum + root.val , sum);
     }
 
+    public int maxSum(TreeNode root) {
+        maxSum(root , 0);
+        System.out.println(max);
+        return max;
+    }
+
+    Integer max = Integer.MIN_VALUE;
+    public void maxSum(TreeNode root , int sum) {
+        if(root == null) {
+            return;
+        }
+        if(root.left == null && root.right == null && sum + root.val > max) {
+            max = sum + root.val;
+        }
+        maxSum(root.left , sum + root.val);
+        maxSum(root.right , sum + root.val);
+    }
+
 
     public static void main(String[] args) {
         PathSum obj = new PathSum();
@@ -37,11 +55,17 @@ public class PathSum {
         TreeNode node1 = new TreeNode(2);
         TreeNode node2 = new TreeNode(3);
         TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(9);
 
         root.left = node1;
         root.right = node2;
         node1.left = node3;
+        node1.right = node5;
 
-        System.out.println(obj.hasPathSum(root , 7));
+        node2.left = node4;
+
+        //System.out.println(obj.hasPathSum(root , 7));
+//        System.out.println(obj.maxSum(root));
     }
 }
