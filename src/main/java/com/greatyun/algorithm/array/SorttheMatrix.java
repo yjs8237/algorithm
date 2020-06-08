ecacf6cdc5b83dd035a34f7dbd66e6e1568ba739
greatyun <yjs8237@gmail.com>
2020-06-08 10:51:19 +0900

    커밋

--- src/main/java/com/greatyun/algorithm/array/SorttheMatrix.java
+++ src/main/java/com/greatyun/algorithm/array/SorttheMatrix.java
@@ -80,15 +80,53 @@ public class SorttheMatrix {
         System.out.println(list.get(0)  + " " + list.get(list.size()-1));
     }
 
+    static void plusMinus(int[] arr) {
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == 0) {
+                zero++;
+            } else if(arr[i] < 0) {
+                negative++;
+            } else {
+                positive++;
+            }
+        }
+
+        Double ret = (Double.parseDouble(String.valueOf(positive)) / arr.length);
+        System.out.println("" + ret);
+        ret = (Double.parseDouble(String.valueOf(negative)) / arr.length);
+        System.out.println(""+ret);
+        ret = (Double.parseDouble(String.valueOf(zero)) / arr.length);
+        System.out.println(""+ret);
+
+    }
+
+    static void staircase(int n) {
+        int index = 1;
+        for(int i=0; i<n; i++) {
+
+            for(int j=0; j<n-(i+1); j++) {
+                System.out.print(" ");
+            }
+            for(int j=0; j<i+1; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+            index++;
+        }
+
+    }
+
     public static void main(String[] args) {
         SorttheMatrix obj = new SorttheMatrix();
         int [][] mat = { {4,3,2,1} , {5,1,2,4} , {3,6,4,2} , {7,7,7,7}};
 
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-
-
+        SorttheMatrix.staircase(6);
 //        obj.diagonalSort(mat);
-
-        obj.test(new int[]{156873294 ,719583602, 581240736, 605827319, 895647130});
+//        obj.test(new int[]{156873294 ,719583602, 581240736, 605827319, 895647130});
     }
 }
