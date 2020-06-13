0728245b0b12b0e2ba4f0fe246c7047b693d2627
greatyun <yjs8237@gmail.com>
2020-06-13 12:40:25 +0900

    커밋 브랜치 테스트

--- src/main/java/com/greatyun/algorithm/binaryTree/BinaryTreeSearch.java
+++ src/main/java/com/greatyun/algorithm/binaryTree/BinaryTreeSearch.java
@@ -52,6 +52,8 @@ public class BinaryTreeSearch {
             log.info("node data[" + node.getData() + "]");
             inorder(node.getLeft());
             inorder(node.getRight());
+
+
         }
     }
 
